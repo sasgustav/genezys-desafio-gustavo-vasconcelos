e2ff3020ce796522554b4fb72912ae5232e1d02c
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-10-16 11:11:13 -0300

    Refactor NavBar component to improve layout and add mobile menu button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 
 const NavBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="bg-gradient-to-r from-blue-800 via-blue-600 to-blue-500 p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-bold">
-          <Link href="/">Genezys App - Gustavo Vasconcelos - Teste Frontend</Link>
+          <Link href="/">Gustavo - Teste Frontend</Link>
         </div>
         <ul className="hidden md:flex space-x-8">
           <NavItem href="/" label="Home" />
@@ -14,9 +20,38 @@ const NavBar: React.FC = () => {
           <NavItem href="/login" label="Login" />
           <NavItem href="/reset-password" label="Recuperar Senha" />
         </ul>
-        <div className="md:hidden">
-          <MobileMenu />
-        </div>
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-white focus:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div
+        className={`md:hidden ${
+          isOpen ? "block" : "hidden"
+        } transition-all duration-300 ease-in-out`}
+      >
+        <ul className="space-y-4 p-4">
+          <NavItem href="/" label="Home" />
+          <NavItem href="/register" label="Cadastro" />
+          <NavItem href="/login" label="Login" />
+          <NavItem href="/reset-password" label="Recuperar Senha" />
+        </ul>
       </div>
     </nav>
   );
@@ -34,25 +69,6 @@ const NavItem: React.FC<{ href: string; label: string }> = ({
       {label}
     </Link>
   </li>
-);
-
-const MobileMenu: React.FC = () => (
-  <button className="text-white focus:outline-none">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M4 6h16M4 12h16M4 18h16"
-      />
-    </svg>
-  </button>
 );
 
 export default NavBar;
