8a05175b7b8d487930ca61469b8d279649948d05
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-10-16 10:52:53 -0300

    Refactor layout to improve form in page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -39,11 +39,9 @@ const Register = () => {
           toast.error("Endereço não encontrado.");
         }
       } catch (error: unknown) {
-        if (error instanceof Error) {
-          toast.error(error.message);
-        } else {
-          toast.error("An unknown error occurred.");
-        }
+        toast.error(
+          error instanceof Error ? error.message : "Erro desconhecido."
+        );
       }
     }
   };
@@ -63,89 +61,103 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-900 to-blue-700 flex items-center justify-center">
-      <div className="max-w-4xl w-full bg-white shadow-lg rounded-lg p-8 border border-gray-200">
+    <div className="min-h-screen bg-gradient-to-b from-blue-900 to-blue-700 flex items-center justify-center px-4">
+      <div className="max-w-3xl w-full bg-white shadow-lg rounded-lg p-8 border border-gray-200">
         <h1 className="text-3xl font-bold text-gray-800 text-center mb-6">
           Crie sua Conta
         </h1>
         <p className="text-center text-gray-500 mb-8">
-          Preencha o formulário abaixo para criar sua conta.
+          Preencha as informações abaixo para criar sua conta.
         </p>
+
         <Form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="col-span-1 md:col-span-2 lg:col-span-3">
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">
+              Dados do Usuário:
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <Input
                 label="Nome Completo"
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
+              <Input
+                label="E-mail"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                label="Senha"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Input
+                label="Confirmar Senha"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </div>
-            <Input
-              label="E-mail"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              label="Senha"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Input
-              label="Confirmar Senha"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <Input
-              label="CEP"
-              type="text"
-              value={cep}
-              onChange={handleCepChange}
-            />
-            <div className="col-span-1 md:col-span-2">
+          </div>
+
+          <div className="border-t border-gray-300 my-8"></div>
+
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-4">
+              Endereço:
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <Input
+                label="CEP"
+                type="text"
+                value={cep}
+                onChange={handleCepChange}
+              />
               <Input
                 label="Rua"
                 type="text"
                 value={address.street}
                 onChange={() => {}}
               />
+              <Input
+                label="Número"
+                type="text"
+                value={address.number}
+                onChange={(e) =>
+                  setAddress((prev) => ({ ...prev, number: e.target.value }))
+                }
+              />
+              <Input
+                label="Bairro"
+                type="text"
+                value={address.neighborhood}
+                onChange={() => {}}
+              />
+              <Input
+                label="Cidade"
+                type="text"
+                value={address.city}
+                onChange={() => {}}
+              />
+              <Input
+                label="Estado"
+                type="text"
+                value={address.state}
+                onChange={() => {}}
+              />
             </div>
-            <Input
-              label="Número"
-              type="text"
-              value={address.number}
-              onChange={(e) =>
-                setAddress((prev) => ({ ...prev, number: e.target.value }))
-              }
-            />
-            <Input
-              label="Bairro"
-              type="text"
-              value={address.neighborhood}
-              onChange={() => {}}
-            />
-            <Input
-              label="Cidade"
-              type="text"
-              value={address.city}
-              onChange={() => {}}
-            />
-            <Input
-              label="Estado"
-              type="text"
-              value={address.state}
-              onChange={() => {}}
-            />
           </div>
+
           <Button
             label="Cadastrar"
             onClick={() => {}}
-            className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-md mt-6 transition duration-300"
+            className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-md transition duration-300"
           />
         </Form>
+
         <p className="text-center text-gray-500 mt-4">
           Já possui uma conta?{" "}
           <a
